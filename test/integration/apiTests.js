c10a11414b501a5f8b6f0dcdb0e128fdf9bc4e11
Eric Broberg <eric.edulis@gmail.com>
2016-03-11 13:15:58 -0800

    add sinon to frameworks, etc

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,30 @@
+// var assert = chai.assert;
+// var should = chai.should();
+// var expect = chai.expect;
+
+
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
+  before(function () {
+    server = sinon.fakeServer.create();
+  });
+
+  after(function () { server.restore(); });
+
   it('todo.setup receives an array of todos when todo.init is called', function () {
+
+    setupStub = sinon.stub(todo, "setup")
+
+    todo.init()
+
+    server.requests[0].respond(
+      200,
+      { "Content-Type": "application/json" },
+      JSON.stringify([{ name: 'Eat',  done: true},{  name: 'Clean',  done: false}])
+    );
+
+    console.log('XXX', setupStub([{name: 'eat', done: false}]) )
+
   });
 });
--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -3,12 +3,65 @@ var should = chai.should();
 var expect = chai.expect;
 
 describe('the todo.App', function() {
-  describe('the todo object', function(){
 
+
+  beforeEach(function () {
+    //newTodo = new todo.App({el: '#todo-app', items: ['eat', 'clean']});
+  });
+
+  describe('the todo object', function(){
     it('should have all the necessary methods', function(){
+      expect(todo.App.prototype.setDone).to.exist;
+      todo.App.prototype.should.have.property("render");
+      expect(true).to.equal(true);
     });
   });
 });
 
-describe('the todo.util methods', function() {
+describe('', function () {
+  var counter = 0;
+  beforeEach(function () {
+    counter++;
+  });
+
+
+  describe('the todo.util methods', function() {
+    it('should trim whitespace', function () {
+      counter.should.equal(1);
+      expect(todo.util.trimTodoName('test ')).to.equal('test');
+      var hey = todo.util.trimTodoName('hey ');
+      assert.lengthOf(hey, 3, "hey should be 3 long bro");
+      assert.typeOf(hey, 'string', "hey should be a string bro");
+
+    });
+    it('should return a boolean', function () {
+      expect(counter).to.be.a("number");
+      expect(counter).to.equal(2);
+      assert.isBoolean(todo.util.isValidTodoName('clean'), "Should return a boolean");
+    });
+
+    it('should return false for an name of one character', function () {
+      expect(counter).to.equal(3);
+
+      expect(todo.util.isValidTodoName('e')).to.be.false;
+    });
+
+    it('should return true for a name of two or more characters', function () {
+      expect(counter).to.equal(4);
+
+      expect(todo.util.isValidTodoName('eat')).to.be.true;
+    });
+
+    it('should return an incremented id', function () {
+      assert.equal(counter, 5, "Sweeeet");
+      assert.isNotBoolean(counter, "Super");
+
+      var firstId = todo.util.getUniqueId();
+      var secondId = todo.util.getUniqueId();
+      expect(secondId - firstId).to.equal(1);
+      secondId.should.not.equal(firstId);
+    });
+  });
 });
+
+
